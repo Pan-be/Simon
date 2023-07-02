a95f22496838a2aa9ac657af9ec0ad7039bb1315
Konrad <konradbieniek@gmail.com>
2023-07-02 23:39:07 +0200

    added checking answers

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const colorsArr = ["red", "blue", "green", "yellow"]
-const gamePattern = []
-const userClickedPattern = []
+let gamePattern = []
+let userClickedPattern = []
+let started = false
+let level = 0
 
 const animation = (color) => {
 	const flash = $(`#${color}`).addClass("pressed")
@@ -15,11 +17,14 @@ const playSound = (color) => {
 }
 
 const nextSequence = () => {
+	userClickedPattern = []
 	const randomNumber = Math.floor(Math.random() * 4)
 	const randomChosenColour = colorsArr[randomNumber]
 	gamePattern.push(randomChosenColour)
 	$(`#${randomChosenColour}`).fadeOut(100).fadeIn(100)
 	playSound(randomChosenColour)
+	level++
+	$("#level-title").text(`Level ${level}`)
 }
 
 const clickHandler = (e) => {
@@ -27,8 +32,35 @@ const clickHandler = (e) => {
 	userClickedPattern.push(userChosenColor)
 	animation(userChosenColor)
 	playSound(userChosenColor)
+	checkAnswer(userClickedPattern.length - 1)
+}
+
+const checkAnswer = (currentLevel) => {
+	if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+		console.log("success")
+		if (gamePattern.length === userClickedPattern.length) {
+			setTimeout(() => {
+				nextSequence()
+			}, 1000)
+		}
+	} else {
+		console.log("wrong")
+		const sound = new Audio("./sounds/wrong.mp3")
+		sound.play()
+		$("body").addClass("game-over")
+		setTimeout(() => $("body").removeClass("game-over"), 200)
+		$("#level-title").text("Game Over, Press Any Key to Restart")
+		started = false
+		gamePattern = []
+	}
 }
 
 $(".btn").click(clickHandler)
 
-nextSequence()
+$(document).keypress(() => {
+	if (!started) {
+		started = true
+		nextSequence()
+		$("#level-title").text("Level 0")
+	}
+})
