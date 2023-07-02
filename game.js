6f5539fff7d1de82125e2e3cb9b41589c63f4f9a
Konrad <konradbieniek@gmail.com>
2023-07-02 10:56:36 +0200

    handle click

--- game.js
+++ game.js
@@ -1,13 +1,34 @@
 const colorsArr = ["red", "blue", "green", "yellow"]
 const gamePattern = []
+const userClickedPattern = []
+
+const animation = (color) => {
+	const flash = $(`#${color}`).addClass("pressed")
+	setTimeout(() => {
+		flash.removeClass("pressed")
+	}, 100)
+}
+
+const playSound = (color) => {
+	const sound = new Audio(`./sounds/${color}.mp3`)
+	sound.play()
+}
 
 const nextSequence = () => {
 	const randomNumber = Math.floor(Math.random() * 4)
 	const randomChosenColour = colorsArr[randomNumber]
 	gamePattern.push(randomChosenColour)
 	$(`#${randomChosenColour}`).fadeOut(100).fadeIn(100)
-	const sound = new Audio(`./sounds/${randomChosenColour}.mp3`)
-	sound.play()
+	playSound(randomChosenColour)
 }
+
+const clickHandler = (e) => {
+	const userChosenColor = e.target.id
+	userClickedPattern.push(userChosenColor)
+	animation(userChosenColor)
+	playSound(userChosenColor)
+}
+
+$(".btn").click(clickHandler)
 
 nextSequence()
